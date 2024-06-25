b75baaefde58580eb72092df1d56fba6227294f6
Davi Reis <dreisss.dev@gmail.com>
2024-06-25 20:05:51 -0300

    chore: update icons and add window blur

--- front/src-tauri/src/main.rs
+++ front/src-tauri/src/main.rs
@@ -1,14 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use window_vibrancy::apply_mica;
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    apply_mica(&window, Some((18, 18, 18, 125)))
+        .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
