52d1a2f31b02395e9657165a6df41503cdfe8695
Davi Reis <dreisss.dev@gmail.com>
2024-06-26 12:59:56 -0300

    chore: few updates in tauri config

--- front/src-tauri/src/main.rs
+++ front/src-tauri/src/main.rs
@@ -1,14 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
 use window_vibrancy::apply_mica;
 
 fn main() {
-    #[cfg(target_os = "windows")]
-    apply_mica(&window, Some((18, 18, 18, 125)))
-        .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
-
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "windows")]
+            apply_mica(&window, Some(true))
+                .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
